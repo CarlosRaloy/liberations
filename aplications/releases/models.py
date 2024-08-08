628ec23da67a7c312985e091556b59e6941b2289
CarlosRaloy <cgarcia@raloy.com.mx>
2024-08-08 09:45:49 -0600

    Creation of models and admins in apps

--- aplications/releases/models.py
+++ aplications/releases/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from aplications.users.models import Profile
+
+"""
+Notes: Massive changes the boolean 0 is direct and number 1
+is massive changes
+"""
 
 
+class ReleaseModel(models.Model):
+    id_user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    default_code = models.CharField(max_length=20)
+    massive_changes = models.BooleanField(default=0)
+    deleted_parts = models.CharField(max_length=20)
+    before_img = models.FileField(upload_to="img/before")
+    after_img = models.FileField(upload_to="img/after")
+
+    def __str__(self):
+        return self.default_code
--- liberations/urls.py
+++ liberations/urls.py
@@ -13,5 +13,5 @@ from django.conf.urls.static import static
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('', include(('aplications.releases.urls', 'releases'), namespace='settings')),
-                  path('users/', include(('aplications.users.urls', 'users'), namespace='users')),
+                  #path('users/', include(('aplications.users.urls', 'users'), namespace='users')),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
